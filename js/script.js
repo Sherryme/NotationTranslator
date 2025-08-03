406a3c8e2802db9a00161dfa908dc5c2160796bd
Sherry <im.sherry.cf@gmail.com>
2025-08-03 09:46:50 +0800

    fix translate function

--- js/script.js
+++ js/script.js
@@ -2,27 +2,51 @@ var str = document.getElementById("input").value;
 
 function getLetters() {
     str = document.getElementById("input").value;
-    console.log(str);  
+    // console.log(str);
 }
 
 function translateFun() {
     getLetters();
-
-    if(str.match(/[a-zA-Z]/) ){
-        // 替换大写字母
-        var originalStr = str.toUpperCase();
-        var Letters = ["C", "D", "E", "F", "G", "A", "B"];
-        var afterStr;
-        for (let i = 0; i < Letters.length; i++) {
-            afterStr += originalStr.replace(Letters[i], i + 1);
+    // 将输入转换为大写以统一处理
+    var originalStr = str.toUpperCase();
+    var result = []; // 用于存储每个字母对应的数字
+    
+    // 遍历每个字母并替换
+    for (let i = 0; i < originalStr.length; i++) {
+        var char = originalStr[i];
+        switch(char) {
+            case 'C':
+                result.push('1');
+                break;
+            case 'D':
+                result.push('2');
+                break;
+            case 'E':
+                result.push('3');
+                break;
+            case 'F':
+                result.push('4');
+                break;
+            case 'G':
+                result.push('5');
+                break;
+            case 'A':
+                result.push('6');
+                break;
+            case 'B':
+                result.push('7');
+                break;
+            default:
+                // 如果不是简谱字母，保留原字符
+                result.push(char);
         }
-        afterStr=afterStr.replace(/[a-zA-Z]+/g,"");
-        console.log(afterStr);
-        document.getElementById("output").innerText = afterStr;
-    }else{}
+    }
+    
+    var afterStr = result.join(''); // 将数组连接成字符串
+    document.getElementById("output").innerText = afterStr;
 }
 
-function clearFun() {
-    document.getElementById("input").innerHTML = '';
-    document.getElementById("output").innerHTML = '';
+ function clearFun() {
+    document.getElementById("input").value = '';
+    document.getElementById("output").innerText = '';
 }
